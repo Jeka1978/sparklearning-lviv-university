926952ba05009d6394bebedfb1d04215a1496198
bsevgeny@gmail.com <papakita2009>
2017-03-10 15:54:48 +0200

    Initial commit

---
+++ src/main/java/com/lviv/Main.java
@@ -0,0 +1,11 @@
+package com.lviv;
+
+import org.fluttercode.datafactory.impl.DataFactory;
+
+/**
+ * Created by Evegeny on 10/03/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
