72e66a7eea5aa80716849d53db400ca706c9ccfa
bsevgeny@gmail.com <papakita2009>
2017-03-10 16:04:53 +0200

    count lab finished

--- src/main/java/com/lviv/Main.java
+++ src/main/java/com/lviv/Main.java
@@ -1,5 +1,8 @@
 package com.lviv;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.fluttercode.datafactory.impl.DataFactory;
 
 /**
@@ -7,5 +10,11 @@ import org.fluttercode.datafactory.impl.DataFactory;
  */
 public class Main {
     public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf().setAppName("taxi").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+        JavaRDD<String> rdd = sc.textFile("data/taxi/trips.txt");
+        long count = rdd.count();
+        System.out.println("count = " + count);
+
     }
 }
